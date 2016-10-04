b3cb75091a7940c8c8a13b58bb6162466cb618e3
joshuamkite <joshuamkite@gmail.com>
2016-10-05 00:21:08 +0100

    altered build script as leanpub ignores md filenames beginning with '0'

--- ebookbuild.sh
+++ ebookbuild.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 # This script helps to prepare the repo for use with leanpub
-#Recursively copy .md files from the current dir and children to /manuscript. Old versions are overwitten. Orphaned md files in /manuscript are retained
+#destroy /manuscript dir
+rm -rf manuscript
+#recreate /manuscript
+mkdir manuscript
+#Recursively copy .md files from the current dir and children to /manuscript.
 find -iname "*.md" -exec cp {} ./manuscript \;
 #change to manuscript dir
 cd manuscript/
-#create disclaimer to be listed first (dirty hacks R us)
-echo "All credit for content is due to dxw as described at https://github.com/dxw/playbook \n\n This edition was prepared for publication on $(date)" > 00_disclaimer.md
-#prepend '01' to readme so that it comes second in file listing
-mv README.md 01_README.md
+#create disclaimer to be listed first (dirty hacks R us) Leanpub appears to ignore .md files names beginning with 0
+echo "All credit for content is due to dxw as described at https://github.com/dxw/playbook \n\n This edition was prepared for publication on $(date)" > a_copyright_disclaimer.md
+#prepend 'a_' to readme so that it comes second in file listing
+mv README.md a_README.md
 #List chapters and output to list
 ls *.md > Book.txt
 #return to repo root dir to avoid confusion
--- manuscript/a_copyright_disclaimer.md
+++ manuscript/a_copyright_disclaimer.md
@@ -1,3 +1,3 @@
 All credit for content is due to dxw as described at https://github.com/dxw/playbook 
 
- This edition was prepared for publication on Tue  4 Oct 18:08:21 BST 2016
+ This edition was prepared for publication on Wed  5 Oct 00:17:49 BST 2016
